964ea5d1c5943bffc85d52865ad8cf20540e29ed
Slava Barinov <rayslava@gmail.com>
2023-08-16 16:19:47 +0300

    Add app-admin/ocsinventory-agent

---
+++ app-admin/ocsinventory-agent/files/dbconfig.inc.php
@@ -0,0 +1,11 @@
+<?php
+// Name of database
+define("DB_NAME", "ocsweb");
+// Replace localhost by hostname or ip of MySQL server
+define("SERVER_READ","localhost");
+define("SERVER_WRITE","localhost");
+// User allowed to connect to database
+define("COMPTE_BASE","ocs");
+// Password for user
+define("PSWD_BASE","ocs");
+?>
